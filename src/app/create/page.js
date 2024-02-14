77ac9c413cadbe6306bf1dd0005cb4d46cce476f
jae-hwan-kim <woghks7859@gmail.com>
2024-02-15 07:27:15 +0900

    feat: create 후 router.refresh() 로 서버 컴포넌트 렌더링 하기

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,40 +1,43 @@
-'use client';
+"use client";
 
 import { useRouter } from "next/navigation";
 
 export default function Create() {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <form onSubmit={(e)=>{
-            e.preventDefault();
-            const title = e.target.title.value;
-            const body = e.target.body.value;
-            const options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({title, body})
-            }
-            // 캐싱 때문에 root 의 layout에 추가가 안된다. 캐싱으 지우자
-            fetch('http://localhost:9999/topics', options)
-            .then(res=>res.json())
-            .then(result =>{
-                console.log(result);
-                const lastid = result.id;
-                router.push(`/read/${lastid}`)
-            })
-        }}>
-            <p>
-                <input type="text" name="title" placeholder="title" />
-            </p>
-            <p>
-                <textarea name="body" placeholder="body"></textarea>
-            </p>
-            <p>
-                <input type="submit" value="create"/>
-            </p>
-        </form>
-    )
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, body }),
+        };
+        // 캐싱 때문에 root 의 layout에 추가가 안된다. 캐시를 지우자
+        fetch("http://localhost:9999/topics", options)
+          .then((res) => res.json())
+          .then((result) => {
+            // console.log(result);
+            const lastid = result.id;
+            router.push(`/read/${lastid}`);
+            router.refresh();
+          });
+      }}
+    >
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body"></textarea>
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
+  );
 }
