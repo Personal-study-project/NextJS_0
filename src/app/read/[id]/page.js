9d6ee5f9ca2fef11b9b624bb97e8042b049ceca3
jae-hwan-kim <woghks7859@gmail.com>
2024-02-15 08:06:54 +0900

    feat: update 기능 구현

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -2,13 +2,13 @@
 // 'use client';
 
 export default async function Read(props) {
-    const res = await fetch(`http://localhost:9999/topics/${props.params.id}`);
-    const topic = await res.json();
-    console.log(props);
-    return (
-        <>
-            <h2>{topic.title}</h2>
-            {topic.body}
-        </>
-    )
+  const res = await fetch(`http://localhost:9999/topics/${props.params.id}`,
+  {cache:'no-store'});
+  const topic = await res.json();
+  return (
+    <>
+      <h2>{topic.title}</h2>
+      {topic.body}
+    </>
+  );
 }
---
+++ src/app/update/[id]/page.js
@@ -0,0 +1,60 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Update() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const router = useRouter();
+  const params = useParams();
+  const id = params.id;
+
+  useEffect(() => {
+    fetch(`http://localhost:9999/topics/` + id)
+      .then((res) => res.json())
+      .then((result) => {
+        setTitle(result.title);
+        setBody(result.body);
+      });
+  }, []);
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        const options = {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, body }),
+        };
+        // 캐싱 때문에 root 의 layout에 추가가 안된다. 캐시를 지우자
+        fetch("http://localhost:9999/topics/" + id, options)
+          .then((res) => res.json())
+          .then((result) => {
+            const lastid = result.id;
+            router.push(`/read/${lastid}`);
+            router.refresh();
+          });
+      }}
+    >
+      <p>
+        <input type="text" name="title" placeholder="title" value={title}
+            onChange={(e) => setTitle(e.target.value)}
+        />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body" value={body}
+            onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
+  );
+}
