7bc583b11e30159e4d9ca6225d8a4164c6ca0649
jae-hwan-kim <woghks7859@gmail.com>
2024-02-15 08:13:27 +0900

    feat: delete 구현

--- src/app/Control.js
+++ src/app/Control.js
@@ -1,10 +1,11 @@
 "use client";
 
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export function Control() {
   const params = useParams();
+  const router = useRouter();
   const id = params.id;
   console.log("params : ", params);
   return (
@@ -18,7 +19,15 @@ export function Control() {
             <Link href={"/update/" + id}>update</Link>
           </li>
           <li>
-            <input type="button" value="delete"></input>
+            <input type="button" value="delete" onClick={() => {
+              const options = {method: 'DELETE'}
+              fetch('http://localhost:9999/topics/' + id, options)
+              .then(res => res.json())
+              .then(data => {
+                router.push('/');
+                router.refresh();
+              })
+            }}></input>
           </li>
         </>
       ) : null}
